ae7491dbf43b145fce02ecaf26d497f87dbcccc9
Jose Juan <josejuanhernandezgalvez@gmail.com>
2024-01-16 13:18:33 +0000

    docker image updated ->     It is needed to provide an IP to the JAR to be executed

--- token-datamart/src/main/java/com/gitradar/Main.java
+++ token-datamart/src/main/java/com/gitradar/Main.java
@@ -6,10 +6,12 @@ import com.gitradar.storage.databaseviews.DynamoDatabaseView;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length < 1) throw new RuntimeException("IP has not been provided");
+
         DynamoDatabaseView databaseView = new DynamoDatabaseView.Builder()
                 .in("eu-central-2")
                 .port(4566)
-                .ip("http://host.docker.internal")
+                .ip(args[0])
                 .build();
         DatabaseTokenManager suggester = new DatabaseTokenManager(databaseView);
         new TokenSuggesterWebService(suggester)
